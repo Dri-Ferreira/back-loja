b3e445034f53fc3fa0e758c7a747888b339e7015
Adriano Ferreira <adrianoferreira31@gmail.com>
2025-08-31 19:08:20 -0300

    :sparkles: feat: add shopping cart route

---
+++ src/controllers/cart.controller.ts
@@ -0,0 +1,29 @@
+import { RequestHandler } from "express";
+import { cartMountSchema } from "../schemas/cart-mount-schema";
+import { getProduct } from "../services/product.service";
+import { getAbsoluteImageUrl } from "../utils/get-absolute-image";
+
+export const cartMount: RequestHandler = async (req, res) => {
+    // Lógica para montar o carrinho
+    const parseResult = cartMountSchema.safeParse(req.body);
+    if (!parseResult.success) {
+        res.status(400).json({error: 'Array de ids inválido'});
+        return;
+    }
+
+    const { ids } = parseResult.data;
+    let products = [];
+    for (let id of ids) {
+        const product = await getProduct(id); 
+        if (product) {
+            products.push({
+                id: product.id,
+                label: product.label,
+                price: product.price,
+                image: product.images[0] ? getAbsoluteImageUrl(product.images[0]) : null,
+            })
+        }
+    }
+
+    res.json({ error: null, products });
+}
--- src/routes/main.ts
+++ src/routes/main.ts
@@ -2,6 +2,8 @@ import { Router } from "express";
 import * as bannerController from '../controllers/banner.controller';
 import * as productController from '../controllers/products.controller';
 import * as categorytController from '../controllers/category.controller';
+import * as cartController from '../controllers/cart.controller';
+
 
 export const routes = Router();
 
@@ -13,5 +15,7 @@ routes.get('/banners', bannerController.getBanners);
 routes.get('/products', productController.getProducts);
 routes.get('/products/:id', productController.getOneProduct);
 routes.get('/product/:id/related', productController.getRelatedProducts);
-
 routes.get('/category/:slug/metadata', categorytController.getCategoryWithMetadata);
+
+
+routes.post('/cart/mount', cartController.cartMount);
